ac50c2289ba3725420ce71d642e735456b2347de
shuhratkarimov <shuhratkarimov.dev@gmail.com>
2026-02-25 07:33:53 +0500

    phone added

--- controller/shop.ctr.js
+++ controller/shop.ctr.js
@@ -104,9 +104,9 @@ async function getBalancesByAdmin(req, res) {
 async function getBalanceInProvider(req, res) {
     try {
         const doc = await getTotalBalanceInProvider();
-        return doc;
+        return res.json(doc);
     } catch (error) {
-        return error;
+        return res.status(500).json({ message: error.message });
     }
 }
 
